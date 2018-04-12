7e791f1ae1c22194af22a5f9ffd56ebbd0e9eb27
PangHu <djangolee@sina.com>
2018-04-12 09:39:37 +0800

    初级的指针与引用相关

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -11,9 +11,26 @@ int main() {
     // std::cout << i << " x3 " << ri << std::endl;
 
     // 指针
-    int ival = 42;
-    int *p = &ival;
-    std::cout << "point to: " << p << " value: " << *p << std::endl;
+    // int ival = 42;
+    // int *p = &ival;
+    // std::cout << "point to: " << p << " value: " << *p << std::endl;
+
+    // void *
+    // double obj = 3.14, *pd = &obj;
+    // void *pv = &obj;
+    // std::cout << "void *: " << pv << std::endl;
+    // pv = pd;
+    // std::cout << "void *: " << pv << std::endl;
+
+    // 指向指针的引用
+    int i = 42;
+    int *p;
+    int *&r = p;
+    std::cout << "i: " << i << " p: " << p << " r: " << r << std::endl;
+    r = &i;
+    std::cout << "i: " << i << " p: " << p << " r: " << r << std::endl;
+    *r = 0;
+    std::cout << "i: " << i << " p: " << p << " r: " << r << std::endl;
 
     return 0;
 }
